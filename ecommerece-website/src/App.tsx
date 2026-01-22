21dffd2f3907142bc82337cc7d5bba8989a3c16c
Ken <kennethjay0022@gmail.com>
2026-01-22 21:19:08 +0800

    changes

--- ecommerece-website/src/App.tsx
+++ ecommerece-website/src/App.tsx
@@ -1,9 +1,19 @@
-
+import { Routes, Route } from "react-router-dom"
+import Home from './pages/Home.tsx'
+import Collection from './pages/Collection.tsx'
+import About from './pages/About.tsx'
+import Contact from './pages/Contact.tsx'
 
 const App = () => {
   return (
-    <div>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]" >
       
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+      </Routes>
     </div>
   )
 }
