f3fadd65e0a527a496b5c5772c45b38e7cd0cb5d
Ken <kennethjay0022@gmail.com>
2026-01-23 17:40:44 +0800

    changes

--- ecommerece-website/src/components/Navbar.tsx
+++ ecommerece-website/src/components/Navbar.tsx
@@ -7,31 +7,36 @@ const Navbar = () => {
     <div className="flex items-center justify-between py-5 font-medium">
         <img src={assets.logo} alt="logo" className='w-36'/> 
 
-    <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+        <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
 
-    <NavLink to={'/'} className='flex flex-col items-center gap-1'> 
-        <p>HOME</p>
-        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
-    </NavLink>
-    
-    <NavLink to={'/collection'} className='flex flex-col items-center gap-1'> 
-        <p>COLLECTION</p>
-        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
-    </NavLink>
-    
-    <NavLink to={'/about'} className='flex flex-col items-center gap-1'> 
-        <p>ABOUT</p>
-        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
-    </NavLink>
-    
-    <NavLink to={'/contact'} className='flex flex-col items-center gap-1'> 
-        <p>CONTACT</p>
-        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
-    </NavLink>
-    </ul>
+        <NavLink to={'/'} className='flex flex-col items-center gap-1'> 
+            <p>HOME</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+        
+        <NavLink to={'/collection'} className='flex flex-col items-center gap-1'> 
+            <p>COLLECTION</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+        </NavLink>
+        
+        <NavLink to={'/about'} className='flex flex-col items-center gap-1'> 
+            <p>ABOUT</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+        </NavLink>
+        
+        <NavLink to={'/contact'} className='flex flex-col items-center gap-1'> 
+            <p>CONTACT</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+        </NavLink>
+        </ul>
 
-    <div className='flex items-center gap-6'></div>
-        <img src={asset.search_icon} className='w-5 cursor-pointer' alt="icon" />
+        <div className='flex items-center gap-6'>
+            <img src={asset.search_icon} className='w-5 cursor-pointer' alt="icon" /> 
+
+            <div className='group relative'>
+                <img src={asset.profile_icon} alt="profile icon" className='w-5 cursor-pointer'/>
+            </div>
+        </div>
     </div>
   )
 }
