6922fb5735ca9e9d5a0a84253df9de47f8352f44
Ken <kennethjay0022@gmail.com>
2026-01-28 21:40:50 +0800

    Update Collection.tsx

--- ecommerece-website/src/pages/Collection.tsx
+++ ecommerece-website/src/pages/Collection.tsx
@@ -1,3 +1,4 @@
+import { table } from "console"
 
 const Collection = () => {
   return (
@@ -8,3 +9,10 @@ const Collection = () => {
 }
 
 export default Collection
+
+CREATE table if not exists staff (
+  id SERIAL PRIMARY KEY, 
+  dealership_id INTEGER NOT NULL REFERENCES dealerships(id), // foreign key to dealerships table
+  name CHAR(50) NOT NULL, 
+  role CHAR(20) NOT NULL
+)
